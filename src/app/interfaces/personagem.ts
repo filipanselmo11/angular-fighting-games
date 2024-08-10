af640a0142dbef5e6b37aa71a444662df1d55f74
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-08-10 08:24:10 -0400

    feat: add personagem Interface

---
+++ src/app/interfaces/personagem.ts
@@ -0,0 +1,16 @@
+export interface PersonagemRequest {
+  nome: string
+  imagem: string
+  descricao: string
+  jogo: string
+
+}
+
+export interface PersonagemResponse {
+  id: string
+  nome: string
+  imagem: string
+  descricao: string
+  jogo: string
+
+}
--- src/app/services/personagem.service.ts
+++ src/app/services/personagem.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { PersonagemResponse } from '../interfaces/personagem';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonagemService {
 
-  private personaSubject = new BehaviorSubject<any[]>([]);
+  private personaSubject = new BehaviorSubject<PersonagemResponse[]>([]);
   personagens$ = this.personaSubject.asObservable();
   baseUrl = 'https://fighting-games-fastapi.onrender.com/api/v1';
 
@@ -16,7 +18,7 @@ export class PersonagemService {
   }
 
   getPersonagens() {
-    return this.httpClient.get<any[]>(`${this.baseUrl}/personagens`).subscribe(products => {
+    return this.httpClient.get<PersonagemResponse[]>(`${this.baseUrl}/personagens`).subscribe(products => {
       this.personaSubject.next(products);
     });
   }
